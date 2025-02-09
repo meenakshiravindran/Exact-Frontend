728e9df9763566a701b8ffcca42f06c7def4b5ff
jishnupn04 <jishnupneel@gmail.com>
2025-02-09 21:22:21 +0530

    Exam preview refactor and printing functionality

--- src/components/Exam/ExamPreview.jsx
+++ src/components/Exam/ExamPreview.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import { Box, Typography, CircularProgress } from '@mui/material';
+import React from "react";
+import { Box, Typography, CircularProgress, Button } from "@mui/material";
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
 
 const ExamPreview = ({ previewData }) => {
   if (!previewData) {
     return <CircularProgress />;
   }
+  const contentRef = useRef(null);
+  const reactToPrintFn = useReactToPrint({ contentRef });
 
   return (
     <Box mt={3}>
-      <Typography variant="h6">Exam Preview</Typography>
-      <img 
-        src={`data:image/png;base64,${previewData}`} 
-        alt="Exam Preview" 
-        style={{ width: '100%', border: '1px solid #ddd' }}
+      <Box sx={{display:"flex", justifyContent:"space-between"}}>
+        <Typography variant="h6">Exam Preview</Typography>
+        <Button onClick={() => reactToPrintFn()}>Print</Button>
+      </Box>
+
+      <img
+        ref={contentRef}
+        src={`data:image/png;base64,${previewData}`}
+        alt="Exam Preview"
+        style={{ width: "100%", border: "1px solid #ddd" }}
       />
     </Box>
   );
